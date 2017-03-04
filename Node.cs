7ee3fc316fcf48d23be64554798865e912a88ab9
John Paul Depew <jpdepewjp@gmail.com>
2017-03-04 13:02:23 -0500

    First Commit

---
+++ Node.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Node {
+
+    public bool walkable;
+    public Vector3 worldPosition;
+
+    public int gCost;
+    public int hCost;
+
+    public Node(bool _walkable, Vector3 _worldPos)
+    {
+        walkable = _walkable;
+        worldPosition = _worldPos;
+    }
+
+    public int fCost
+    {
+        get
+        {
+            return gCost + hCost;
+        }
+    }
+}
---
+++ Pathfinding.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Pathfinding : MonoBehaviour {
+
+    Grid grid;
+
+    void Awake()
+    {
+        grid = GetComponent<Grid>();
+    }
+
+    void FindPathf(Vector3 startPos, Vector3 targetPos)
+    {
+        Node startNode = grid.NodeFromWorldPoint(startPos);
+        Node targetNode = grid.NodeFromWorldPoint(targetPos);
+
+        List<Node> openSet = new List<Node>();
+        HashSet<Node> closedSet = new HashSet<Node>();
+
+        openSet.Add(startNode);
+
+        while(openSet.Count > 0)
+        {
+            Node currentNode = openSet[0];
+            for (int i = 1; i< openSet.Count; i++)
+            {
+                if(openSet[i].fCost < currentNode.fCost)
+                {
+                    currentNode = openSet[i];
+                }
+            }
+        }
+    }
+}
